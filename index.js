56417ae73666e739c8b93d6942532b0e6ff9ceca
Faris <125892193+XxFARISxX@users.noreply.github.com>
2024-07-21 23:07:40 +0300

    add a hover affect that will change the color of skelters when a pointer moves over them.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// this function adds a grid made of divs i will call "helter/skelter", inside the div i call "canvas".
+// this function adds a grid made of divs i will call "helter/skelter", inside the div called "canvas".
 function grid() {
 
     const canvas = document.querySelector("#canvas");
 
     /*lets say you need a 16x16 grid. the for loop below will run 16 times. 
-      creating 16 long vertical divs, that i will call "helters".
-      and 16 divs, that i will call "skelters, inside of each helter.*/
+      creating 16 long vertical divs, called "helters".
+      and 16 divs, called "skelters", inside of each helter.*/
     for (let i = 0; i < 16; i++) {
         const helter = document.createElement("div");
 
@@ -16,7 +16,7 @@ function grid() {
 
         /*this loop runs 16 times each time the helter loop runs once. 
           since it's inside of the helter loop. 
-          this will create 16 divs, that i will call "skelters", inside each helter.*/
+          it will create 16 skelters, inside each helter.*/
         for (let i = 0; i < 16; i++) {
             const skelter = document.createElement("div");
 
@@ -24,8 +24,21 @@ function grid() {
 
             //this appends the skelters inside each helter.
             helter.appendChild(skelter);
+
+            /*this function and eventListener below it adds color to a skelter, 
+              when a mouse pointer moves onto it.
+              acting as a pencil/marker or a "pen", on the whole canvas.
+              more colors will be added later, and the whole setup will change.*/
+            function pen () {
+              skelter.style.background = "blue";
+              skelter.style.color = "blue";
+            };
+            skelter.addEventListener("mouseenter", pen);
+
         };
+
     };
+
 };
 
 /*this console.logs the grid function. so whenever the page is loaded,

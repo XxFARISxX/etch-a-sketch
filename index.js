6bb03728272e08f2cbaf65e1809573d70b299a01
Faris <125892193+XxFARISxX@users.noreply.github.com>
2024-07-24 14:45:08 +0300

    remove console.log(grid()); add document.body.onload = grid();

--- index.js
+++ index.js
@@ -40,40 +40,39 @@ function grid(num = 16) {
     creating 16 long vertical divs, called "helters".
     and 16 divs, called "skelters", inside of each helter.*/
   for (let i = 0; i < gridSize; i++) {
-      const helter = document.createElement("div");
+    const helter = document.createElement("div");
 
-      helter.className = "helter";
+    helter.className = "helter";
 
-      //this will append each helter inside the canvas.
-      canvas.appendChild(helter);
+    //this will append each helter inside the canvas.
+    canvas.appendChild(helter);
 
-      /*this loop runs 16 times each time the helter loop runs once. 
-        since it's inside of the helter loop. 
-        it will create 16 skelters, inside each helter.*/
-      for (let i = 0; i < gridSize; i++) {
-          const skelter = document.createElement("div");
+    /*this loop runs 16 times each time the helter loop runs once. 
+      since it's inside of the helter loop. 
+      it will create 16 skelters, inside each helter.*/
+    for (let i = 0; i < gridSize; i++) {
+      const skelter = document.createElement("div");
 
-          skelter.className = "skelter";
+      skelter.className = "skelter";
 
-          //this appends the skelters inside each helter.
-          helter.appendChild(skelter);
+      //this appends the skelters inside each helter.
+      helter.appendChild(skelter);
 
-          /*this function and eventListener below it adds color to a skelter, 
-            when a mouse pointer moves onto it.
-            acting as a pencil/marker or a "pen", on the whole canvas.
-            more colors will be added later, and the whole setup will change.*/
-            function pen () {
-              skelter.style.background = "black";
-            };
-            skelter.addEventListener("mouseenter", pen);
-
+      /*this function and eventListener below it adds color to a skelter, 
+        when a mouse pointer moves onto it.
+        acting as a pencil/marker or a "pen", on the whole canvas.
+        more colors will be added later, and the whole setup will change.*/
+      function pen () {
+        skelter.style.background = "black";
       };
+
+      skelter.addEventListener("mouseenter", pen);
+
+    };
 
   };
 
 };
 
-/*this console.logs the grid function. so whenever the page is loaded,
-  a grid of 16x16 is automatically built. like it was always there.
-  i'll try to figure out a better way to do this. later.*/
-console.log(grid());
+/*when body loads, a grid of 16x16 is automatically built. like it was always there.*/
+document.body.onload = grid();
